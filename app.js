09a7e85a88769b70386f7d7491b4a8f23aded0c4
dinith72 <jdinith72@gmail.com>
2019-01-28 23:23:29 +0530

    done upto lesson 7

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add_data');
+const btnDelete = document.querySelector('#btnDelete');
 
 // create element & render cafe
 function renderCafe(doc){
@@ -17,8 +19,34 @@ function renderCafe(doc){
 }
 
 // getting data
-db.collection('cafes').get().then(snapshot => {
+db.collection('cafes').where('city' , '==' , 'gampaha').orderBy('name').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
-        renderCafe(doc);
+        // renderCafe(doc);
+        console.log(doc.data());
     });
 });
+
+// gett data querying 
+// db.collection('cafes').where('city' , '<','h').get().then(snapshot => {
+//         snapshot.docs.forEach(doc => {
+//             // renderCafe(doc);
+//             console.log(doc.data());
+//         });
+//     });
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    // alert('submit clicked');
+    var cafe = {
+        name: 'burgher king',
+        city: 'galle'
+    };
+    db.collection('cafes').add(cafe);
+
+});
+
+btnDelete.addEventListener('click',(event)=>{
+    alert('delete clicked');
+    db.collection('cafes').doc('sEXQ4OU4F7FQdqeuPB5E').delete();
+});
+
